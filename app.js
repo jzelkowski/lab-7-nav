7b08f1c0b135bc9a0eb4c9daf35a5525e7af18fe
Jamie Zelkowski <jewelrybyjamiez@gmail.com>
2016-04-05 21:09:45 -0400

    content added

---
+++ app.js
@@ -0,0 +1,7 @@
+  // var test = document.getElementById("links");
+$( "nav" ).mouseover(function() {
+  $( this ).fadeTo( "slow", 0.33 );
+});
+$( "nav" ).mouseleave(function() {
+  $( this ).fadeTo( "slow", 1 );
+});
